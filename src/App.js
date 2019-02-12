187944a4600afb8cdde53d487f48e3b32778c25d
sujitnair51 <sujitnair51@gmail.com>
2019-02-12 16:55:05 -0500

    added new text message in App.js file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Changed again!
+           Changed from the feature branch!
           </p>
           <a
             className="App-link"
